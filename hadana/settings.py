d3ad64c1f5d930b3ea6abb1bdddcd5cb8f4e99e2
abdallahomar1 <70544642+abdallahomar1@users.noreply.github.com>
2021-01-02 11:12:34 +0200

    add a datebase

--- hadana/settings.py
+++ hadana/settings.py
@@ -78,15 +78,10 @@ WSGI_APPLICATION = 'hadana.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hadana',
-        'USER': 'postgres',
-        'PASSWORD': '002233',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
